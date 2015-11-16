d05ad71698e7886f8ea782f817e019d71808e07a
c475 <danjamesbond@gmail.com>
2015-11-15 17:28:40 -0800

    .

--- mediacenter/urls.py
+++ mediacenter/urls.py
@@ -4,12 +4,12 @@ from frontend.views import *
 
 urlpatterns = patterns('',
     # main
-    url(r'^$', Index.as_view()),
-    url(r'^index/$', Index.as_view()),
-
     url(r'^rooms/$', RoomSelect.as_view()),
 
     # auth
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^login/$', Login.as_view()),
+
+    url(r'^index/$', Index.as_view()),
+    url(r'^$', Index.as_view()),
 )
