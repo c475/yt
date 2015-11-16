b3f7ae6c00727da773117dd04f077a02021f708d
c475 <danjamesbond@gmail.com>
2015-11-15 19:01:33 -0800

    .

--- backend/controllers/YoutubeController.py
+++ backend/controllers/YoutubeController.py
@@ -61,10 +61,8 @@ class YoutubeController(object):
                     ret[field.name] = None
                 else:
                     ret[field.name] = field_value.strftime("%Y-%m-%d %H:%M:%S")
-            elif isinstance(getattr(model_instance, field.name), User):
+            elif isinstance(getattr(model_instance, field.name), (User, Room)):
                 ret[field.name] = self.serialize(getattr(model_instance, field.name))
-            elif isinstance(field, Room):
-                ret[field.name] = field.name
             else:
                 ret[field.name] = field_value
 
