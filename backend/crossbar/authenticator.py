a1889f3fee759418d00e7aad53f356bee63dfffc
c475 <danjamesbond@gmail.com>
2015-12-25 17:14:24 -0800

    .

--- backend/crossbar/authenticator.py
+++ backend/crossbar/authenticator.py
@@ -3,6 +3,26 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
 from autobahn.wamp.exception import ApplicationError
 
+from sys import path
+import os
+
+paths = [
+    '/srv/',
+    '/srv/mediacenter',
+    '/srv/frontend',
+    '/srv/backend',
+    '/srv/backend/crossbar'
+]
+
+for p in paths:
+    if p not in path:
+        path.append(p)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from django import setup
+setup()
+
 
 class Authenticator(ApplicationSession):
 
