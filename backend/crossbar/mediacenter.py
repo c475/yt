5e6fe5af3bbd76c48f4353a2dc73ae32683c82a3
c475 <danjamesbond@gmail.com>
2015-12-26 11:46:06 -0800

    .

--- backend/crossbar/mediacenter.py
+++ backend/crossbar/mediacenter.py
@@ -87,6 +87,7 @@ class Mediacenter(ApplicationSession):
     def onChallenge(self, challenge):
         print('doing challenge thing')
         print(challenge.__dict__)
+        return 'challenge'
 
     @inlineCallbacks
     def onJoin(self, details):
