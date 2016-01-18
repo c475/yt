0a23a9df183dcef1d4bbd8c6e44ee0eee7bcffeb
c475 <danjamesbond@gmail.com>
2016-01-17 21:23:29 -0800

    .

--- backend/models/Room.py
+++ backend/models/Room.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.core.validators import RegexValidator
 from django.db import models
+from backend.models import User
 
 alnum = RegexValidator(r'^[0-9a-zA-Z]+$', 'Only alphanumeric characters are allowed.')
 
@@ -9,6 +10,10 @@ class Room(models.Model):
     creator = models.ForeignKey('backend.User')
     date_created = models.DateTimeField(default=datetime.now, blank=True)
     description = models.CharField(max_length=500)
+
+    @property
+    def user_total(self):
+        return User.objects.filter(current_room_id=self.id).count()
 
     class Meta:
         app_label = 'backend'
