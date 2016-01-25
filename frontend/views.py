3e44c1818add8252bb2d572e688ffec6fba35dd6
c475 <danjamesbond@gmail.com>
2016-01-24 17:52:40 -0800

    .

--- frontend/views.py
+++ frontend/views.py
@@ -68,6 +68,9 @@ class RoomSelect(LoggedInMixin, ListView):
     template_name = 'rooms-listing.html'
     paginate_by = 20
 
+    def get_queryset(self, *args, **kwargs):
+        return super(RoomSelect, self).get_queryset(*args, **kwargs).order_by('user_total')
+
 
 class RoomCreate(LoggedInMixin, CreateView):
     model = Room
