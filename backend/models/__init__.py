4c24f34f9d1208ce49ed1ac89819d832829a7eeb
c475 <danjamesbond@gmail.com>
2016-01-27 23:09:34 -0800

    .

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,3 +2,4 @@ from backend.models.Video import Video
 from backend.models.User import User
 from backend.models.Chat import Chat
 from backend.models.Room import Room
+from backend.models.Twitch import Twitch
